eb71de071ce6ac8cecdf09d53089f0c65467e97a
DamesNicholas-FS <85197893+DamesNicholas-FS@users.noreply.github.com>
2023-06-04 16:37:56 -0400

    Update page.js
    changing title page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
+    <>
+    <title>My First Next JS App</title>
     <div className="min-h-screen flex justify-center items-center flex-col gap-7 select-none">
       <h1 className="flex justify-center items-center gap-7 text-8xl font-bold border-b pb-2 px-8 ">Welcome To <img className="invert" src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg" alt="" /></h1>
       <h2 className="text-xl font-light">Made with <span className="animate-pulse">❤️</span> & Next.js + Tailwind CSS + React</h2>
     </div>
+    </>
   )
 }
