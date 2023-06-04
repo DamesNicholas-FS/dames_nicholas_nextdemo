c839d5a51d6a3ea9bcaad507d5cf7afc35ca804a
DamesNicholas-FS <85197893+DamesNicholas-FS@users.noreply.github.com>
2023-06-04 17:38:55 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,12 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center flex-col gap-7">
-      <h1 className="flex justify-center items-center gap-7 text-8xl font-bold border-b pb-2 px-8 ">Welcome To <img className="invert" src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg" alt="" /></h1>
-      <h2 className="text-xl font-light">This side was made with <span className="animate-pulse">❤️</span> & Next.js + Tailwind CSS + React</h2>
-    </div>
+    <>
+      <title>First Static Next App</title>
+      <div className="min-h-screen flex justify-center items-center flex-col gap-7">
+        <h1 className="flex justify-center items-center gap-7 text-8xl font-bold border-b pb-2 px-8 ">Welcome To <img className="invert" src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg" alt="" /></h1>
+        <h2 className="text-xl font-light">This side was made with <span className="animate-pulse">❤️</span> & Next.js + Tailwind CSS + React</h2>
+      </div>
+    </>
   )
 }
