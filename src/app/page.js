eedc45483a94e13d70bfd8e15e7ad7fb8434c6f6
DamesNicholas-FS <85197893+DamesNicholas-FS@users.noreply.github.com>
2023-06-25 22:59:40 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -79,8 +79,50 @@ export default function Home() {
       <img className="p-4 rounded" src="https://www.qed42.com/sites/default/files/styles/featured_image/public/2018-11/react-native.png?itok=lj4A4r0I" alt="react native with andriod and ios" />
       <p><a className="w-auto flex justify-center italic" href="https://www.qed42.com/insights/coe/javascript/react-native-fabric-why-am-i-so-excited">Courtesy of: QED42</a></p>
     </div>
-    
+
+    <div className="border p-6 rounded-lg bg-white text-black w-5/12 mb-9">
+
+<div className="flex items-center justify-between mb-10">
+  <div className="flex items-center gap-3">
+    <img className="rounded-full shadow w-14" src="https://media.licdn.com/dms/image/C5603AQHSojSDrL2BbQ/profile-displayphoto-shrink_800_800/0/1574342692271?e=1691625600&v=beta&t=elRyjom_c_4ELSXogtaGv4_bdAXRodvDNk4ouwTehxw" alt="" />
+      <ul>
+        <li className="font-bold">Nicholas Dames</li>
+        <li className="font-light text-xs text-gray-500">FullSail University Student - Web Development</li>
+      </ul>
+  </div>
+  <p className="font-light text-xs text-gray-500">Thursday, June 24<sup>th</sup>, 2023
+</p>
+</div>
+    <h3 className="font-bold text-xl py-2">Password Hashing</h3>
+      <ul>
+        <li className="underline text-blue-700"> &gt;	 <a href="https://nordpass.com/blog/password-hash/">What is password hashing?</a> </li>
+        <li className="underline text-blue-700"> &gt;	 <a href="https://www.okta.com/blog/2019/03/what-are-salted-passwords-and-password-hashing/#:~:text=Password%20hashing%20is%20defined%20as,unintelligible%20to%20the%20bad%20actor.">What are Salted Passwords and Password Hashing?</a> </li>
+        <li className="underline text-blue-700"> &gt;	 <a href="https://auth0.com/blog/hashing-passwords-one-way-road-to-security/">Hashing Passwords: One-Way Road to Security</a> </li>
+        <li className="underline text-blue-700"> &gt;	 <a href="https://www.youtube.com/watch?v=--tnZMuoK3E">Password Hashing, Salts, Peppers | Explained!</a> </li>
+        <li className="underline text-blue-700"> &gt;	 <a href="https://www.linkedin.com/pulse/secure-user-passwords-adding-salt-pepper-sherlin-oneeta-bastin/">Secure User Passwords by Adding Salt and Pepper</a> </li>
+      </ul>
+      <img className="p-4 rounded" src="https://media.licdn.com/dms/image/C4E12AQF0tXc7dj0kNw/article-cover_image-shrink_423_752/0/1572907990230?e=1693440000&v=beta&t=rExpNlDxhyGRnH9OQtMWMJViu7447icmM_Qz5h6Ymww" alt="Secure User Passwords by Adding Salt and Pepper" />
+      <p><a className="w-auto flex justify-center italic" href="https://www.linkedin.com/pulse/secure-user-passwords-adding-salt-pepper-sherlin-oneeta-bastin/">Courtesy of: LinkedIn</a></p>
+      <p className="pt-5 text-justify">Password hashing is a crucial part of secure data storage&#44; primarily used to protect sensitive user data like passwords&#46; It involves transforming a password into an unrecognizable string of characters&#44; the hash&#44; using a hash function&#46; This hash is stored instead of the actual password&#46; When the user enters their password to log in&#44; it is transformed using the same hash function and compared to the stored hash&#46; If they match&#44; the password is correct and the user is granted access&#46;</p>
+        <p className="py-3 font-bold">Password hashing serves several key purposes for security&#58;</p>
+          <ul className="py-3">
+            <li className="py-1 pl-2 font-light">	&bull; It protects against data breaches&#58; In case a system&#39;s user data is compromised&#44; the attacker obtains the hashes of the passwords&#44; not the actual passwords&#46; Since hash functions are designed to be irreversible&#44; it is extremely difficult for the attacker to deduce the original password from the hash&#46;</li>
+            <li className="py-1 pl-2 font-light">	&bull; It protects against &#34;rainbow table&#34; attacks&#58; Rainbow tables are precomputed tables for reversing cryptographic hash functions and can be used to crack password hashes&#46; However&#44; if the hashing process includes &#34;salting&#34;&#44; rainbow tables are ineffective&#46;</li>
+          </ul>
+          <p className="py-3">Salting is a technique that further enhances the security of hashed passwords&#46; A salt is a random string of characters that is appended to the password before the hash is computed&#46; The salt is then stored along with the hash&#46; This means that even if two users have the same password&#44; their hashed passwords will differ due to the different salts used&#46; Salting offers two main benefits&#58;</p>
+          <ul className="py-3">
+            <li className="py-1 pl-2 font-light">	&bull; It prevents pre-computation attacks&#58; Since each user&#39;s password is salted with a unique value&#44; an attacker cannot use a precomputed table of hashes&#44; like a rainbow table&#44; to crack the passwords&#46;</li>
+            <li className="py-1 pl-2 font-light">	&bull; It prevents identical passwords from having identical hashes&#58; Without salts&#44; the same password would always produce the same hash&#44; making it easier for attackers to guess passwords by noticing patterns in the hashes&#46;</li>
+          </ul>
+          <p className="py-3">There are many different hashing algorithms that can be used for password hashing&#44; including&#58;</p>
+          <ul className="py-3">
+            <li className="py-1 pl-2 font-light">	&bull; Bcrypt&#58; A widely used password hashing function that automatically handles the generation of salts&#46;</li>
+            <li className="py-1 pl-2 font-light">	&bull; Scrypt&#58; Similar to Bcrypt but is designed to be more resistant to hardware accelerated attacks because it requires larger amounts of memory to compute&#46;</li>
+            <li className="py-1 pl-2 font-light">	&bull; Argon2&#58; A relatively new algorithm that won the Password Hashing Competition in 2015&#46; It offers a high degree of security and flexibility&#46;</li>
+          </ul>
+        <p className="py-3">The aim of password hashing (and salting) is to make it as difficult and time-consuming as possible for an attacker to determine the original password even if they have obtained the hash&#46;</p>
     </div>
+</div>
     </>
   )
 }
